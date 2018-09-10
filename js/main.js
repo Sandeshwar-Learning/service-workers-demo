ed335f4633ec8ae1e31d80ca21b28cc21a2d5450
sandyneshore <sandeshwar.s@neshore.com>
2018-09-10 21:07:05 +0530

    Register service worker file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-if (navigator.serviceWorker) {
-    console.log('service worker available');
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', _ => {
+        // Register service worker
+        navigator.serviceWorker
+            .register('./js/service-worker.js')
+            .then(_ => console.log('Service worker registered'))
+            .catch(err => console.log('Failed to registered service worker', err));
+    });
     
 }
