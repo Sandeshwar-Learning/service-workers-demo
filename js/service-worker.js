e9b1e36b2a610fd3f12bab418626953a4b95993d
sandyneshore <sandeshwar.s@neshore.com>
2018-09-10 21:17:17 +0530

    Listen to the activate event

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,11 @@
+const cacheName = 'fullSiteCache';
+
 // Service worker install event
 self.addEventListener('install', e => {
     console.log('Service worker installed');    
+});
+
+// Service worker activate event
+self.addEventListener('activate', e => {
+    console.log('Service worker activated');
 });
